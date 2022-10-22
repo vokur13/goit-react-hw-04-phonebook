a11b662e68470997a42ae145378e8ffacf34dd99
vokur13 <vokur13@gmail.com>
2022-10-22 17:57:05 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
